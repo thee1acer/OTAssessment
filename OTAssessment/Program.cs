1d402fe1ce38c8ca5d781669c85e8ba4f5110140
= <=>
2025-04-16 14:38:13 +0200

    database scaffold

--- OTAssessment/Program.cs
+++ OTAssessment/Program.cs
@@ -1,25 +1,41 @@
+using OT.Assessment.Database;
+using OT.Assessment.Database.Helpers;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddOptions<ConnectionString>().BindConfiguration("DATABASE");
+builder.Services.AddDbContext<OTAssessmentContext>((provider, options) =>
+{
+    var connectionDetails = provider.GetRequiredService<IOptions<ConnectionString>>();
+    var connectionString = ConnectionStringBuilder.BuildConnectionString(connectionDetails.Value);
+
+    options.UseSqlServer(connectionString, ops => ops.EnableRetryOnFailure());
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<OTAssessmentContext>();
+
+    //migrate changes
+}
+
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
