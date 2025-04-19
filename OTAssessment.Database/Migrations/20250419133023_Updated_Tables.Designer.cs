47827b1cda8af4399a39e5c33828ecf0c0c1c957
= <=>
2025-04-19 15:31:23 +0200

    updated tables

--- OTAssessment.Database/Migrations/20250419133023_Updated_Tables.Designer.cs
+++ OTAssessment.Database/Migrations/20250419133023_Updated_Tables.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OT.Assessment.Database;
 
@@ -11,9 +12,11 @@ using OT.Assessment.Database;
 namespace OT.Assessment.Database.Migrations
 {
     [DbContext(typeof(OTAssessmentContext))]
-    partial class OTAssessmentContextModelSnapshot : ModelSnapshot
+    [Migration("20250419133023_Updated_Tables")]
+    partial class Updated_Tables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ OTAssessment.Database/Migrations/20250419133023_Updated_Tables.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace OT.Assessment.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class Updated_Tables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AccessLevels",
+                keyColumn: "Id",
+                keyValue: new Guid("18710e16-32d1-4aad-b849-f644c4d0569d"));
+
+            migrationBuilder.DeleteData(
+                table: "AccessLevels",
+                keyColumn: "Id",
+                keyValue: new Guid("24e5642f-c968-49dc-8830-4ff6548f8651"));
+
+            migrationBuilder.DeleteData(
+                table: "AccessLevels",
+                keyColumn: "Id",
+                keyValue: new Guid("da6bb716-7a7f-4528-90e5-f749feac552c"));
+
+            migrationBuilder.InsertData(
+                table: "AccessLevels",
+                columns: new[] { "Id", "AccesType", "AccessDescription" },
+                values: new object[,]
+                {
+                    { new Guid("0f2e9e4a-b535-4d6a-855a-a5d3d761eb8d"), "Administrator", "This is for admin" },
+                    { new Guid("1ad9aced-ba4b-4626-abb9-ccec1fd1beca"), "QaTester", "This is for qa-testers" },
+                    { new Guid("7a4b1186-41bb-4079-99da-62a22b08c1a2"), "Developer", "This is for developers" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AccessLevels",
+                keyColumn: "Id",
+                keyValue: new Guid("0f2e9e4a-b535-4d6a-855a-a5d3d761eb8d"));
+
+            migrationBuilder.DeleteData(
+                table: "AccessLevels",
+                keyColumn: "Id",
+                keyValue: new Guid("1ad9aced-ba4b-4626-abb9-ccec1fd1beca"));
+
+            migrationBuilder.DeleteData(
+                table: "AccessLevels",
+                keyColumn: "Id",
+                keyValue: new Guid("7a4b1186-41bb-4079-99da-62a22b08c1a2"));
+
+            migrationBuilder.InsertData(
+                table: "AccessLevels",
+                columns: new[] { "Id", "AccesType", "AccessDescription" },
+                values: new object[,]
+                {
+                    { new Guid("18710e16-32d1-4aad-b849-f644c4d0569d"), "Administrator", "This is for admin" },
+                    { new Guid("24e5642f-c968-49dc-8830-4ff6548f8651"), "Developer", "This is for developers" },
+                    { new Guid("da6bb716-7a7f-4528-90e5-f749feac552c"), "QaTester", "This is for qa-testers" }
+                });
+        }
+    }
+}
