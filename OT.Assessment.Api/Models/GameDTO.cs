8ec05526e17584e2d9b056021cbc7044022cc1c4
= <=>
2025-04-22 00:07:19 +0200

    corrected minor issue

--- OT.Assessment.Api/Models/GameDTO.cs
+++ OT.Assessment.Api/Models/GameDTO.cs
@@ -5,9 +5,9 @@ public class GameDTO
     public Guid Id { get; set; }
     public required string Name { get; set; }
     
-    public required Guid ThemeName { get; set; }
+    public required string ThemeName { get; set; }
     public virtual required ThemeDTO Theme { get; set; }
 
-    public int ProviderId { get; set; }
+    public Guid ProviderId { get; set; }
     public virtual required ProviderDTO Provider { get; set; }    
 }
--- OT.Assessment.Database/Models/Game.cs
+++ OT.Assessment.Database/Models/Game.cs
@@ -5,9 +5,9 @@ public class Game
     public Guid Id { get; set; }
     public required string Name { get; set; }
     
-    public required Guid ThemeName { get; set; }
+    public required string ThemeName { get; set; }
     public virtual required Theme Theme { get; set; }
 
-    public int ProviderId { get; set; }
+    public Guid ProviderId { get; set; }
     public virtual required Provider Provider { get; set; }      
 }
